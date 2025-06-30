0c0cd52dca896f7eaedf9b610b30bdd0902a0e32
Murugi <123889544+murugii@users.noreply.github.com>
2025-06-30 10:42:15 +0300

    Add files via upload

--- Login.py
+++ Login.py
@@ -1,24 +1,32 @@
-from selenium.webdriver.common.by import By
+import time
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
-driver.maximize_window()
+#test the herokuapp with the url: https://the-internet.herokuapp.com/login
+url = "https://the-internet.herokuapp.com/login"
+#declare the username and password variables
 username = "tomsmith"
 password = "SuperSecretPassword!"
-login_url = "https://the-internet.herokuapp.com/login"
-driver.get(login_url)
-username_field = driver.find_element(By.ID, value="username")
-password_field = driver.find_element(By.ID, value = "password")
 
+driver.get(url)
+driver.maximize_window()
+time.sleep(4)
+username_field = driver.find_element(By.ID, "username")
+password_field = driver.find_element(By.ID, "password")
+
+#pass the username and passwords
 username_field.send_keys(username)
 password_field.send_keys(password)
 
-login_button = driver.find_element(By.CLASS_NAME, value = "radius")
-assert not login_button.get_attribute("disabled")
-login_button.click()
+#find and click the login button
+driver.find_element(By.CSS_SELECTOR, ".fa.fa-2x.fa-sign-in").click()
 
-login_success = driver.find_element(By.CLASS_NAME, value = "flash")
-assert "You logged into a secure area!" in login_success.text
+# verify that the login was successful by, e.g, confirming that the "Secure Area" text on the landing page is visible
+login_success = driver.find_element(By.XPATH, "//h2[normalize-space()='Secure Area']")
+assert login_success.text == "Secure Area"
+time.sleep(3)
 
-
+# find and click the logout button
+driver.find_element(By.XPATH, "//i[@class='icon-2x icon-signout']").click()
+time.sleep(4)
